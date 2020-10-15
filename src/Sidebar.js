66d14fdda70a01839e641822900aced44a077263
vistritpandey <vistritpandey@gmail.com>
2020-10-15 17:52:51 +0530

    f

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Sidebar.css'
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import AddIcon from "@material-ui/icons/Add"
 
 
 function Sidebar() {
@@ -9,6 +10,13 @@ function Sidebar() {
             <div className="sidebar__top">
                 <h3>Chat Groups</h3>
                 <ExpandMoreIcon />
+            </div>
+            <div className="sidebar__groups">
+                <div className="sidebar__header">
+                    <ExpandMoreIcon />
+                    <h4>Groups</h4>
+                </div>
+                <AddIcon className= "sidebar__addGroup" />
             </div>
         </div>
     )
