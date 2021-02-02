31eccf1fefb62c515ab6f61b5ebece11b34e88fc
Vistrit <56611775+VistritPandey@users.noreply.github.com>
2021-02-02 07:16:26 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAx5_73RWK1G_bcItj3_FnmK5bkAXhJZtI",
-    authDomain: "vistrit-chat.firebaseapp.com",
-    databaseURL: "https://vistrit-chat.firebaseio.com",
-    projectId: "vistrit-chat",
-    storageBucket: "vistrit-chat.appspot.com",
-    messagingSenderId: "978901820727",
-    appId: "1:978901820727:web:92b8f3d58c02fd0e00e6b2",
-    measurementId: "G-EG66KK09DW"
+const firebaseConfig = {    
+    // use your own config file
   };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore()
